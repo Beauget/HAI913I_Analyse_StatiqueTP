6cedcc2a028ef0dacb31a84c09e48d477df173b9
Beauget <azentiaytbpro@gmail.com>
2021-09-30 15:18:17 +0200

    Ajout fenetre + commande base

---
+++ YP2/src/Fenetre.java
@@ -0,0 +1,60 @@
+package src;
+
+
+
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Fenetre extends JFrame {
+	
+	
+	
+	// Bouton pour les questions
+	private JPanel pan = new JPanel();
+	private JButton bouton1 = new JButton("Nombre de classe");
+	private JButton bouton2 = new JButton("Nombre	de lignes de code de l’application");
+	private JButton bouton3 = new JButton("Nombre total de méthodes de l'application");
+	private JButton bouton4 = new JButton("Nombre total de packages de l'application");
+	private JButton bouton5 = new JButton("Nombre moyen de méthodes par classe");
+	private JButton bouton6 = new JButton("Nombre moyen de lignes de code par méthode");
+	private JButton bouton7 = new JButton("Nombre moyen d'attributs par classe");
+	private JButton bouton8 = new JButton("Les 10% des classes qui possèdent le plus grand nombre de méthodes");
+	private JButton bouton9 = new JButton("Les 10% des classes qui possèdent le plus grand nombre d'attributs");
+	private JButton bouton10 = new JButton("Les classes qui font partie en même temps des deux catégories précédentes");
+	private JButton bouton11 = new JButton("Les 10% des méthodes qui possèdent le plus grand nombre de lignes de code (par classe)");
+	private JButton bouton12 = new JButton("Le nombre	maximal	de paramètres par rapport à toutes les méthodes	de l’application.");
+	
+
+	public Fenetre() {
+		this.setTitle("AST Visitor");
+		this.setSize(800,800);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
+
+		pan.add(bouton1);
+		pan.add(bouton2);
+		pan.add(bouton3);
+		pan.add(bouton4);
+		pan.add(bouton5);
+		pan.add(bouton1);
+		pan.add(bouton6);
+		pan.add(bouton7);
+		pan.add(bouton8);
+		pan.add(bouton9);
+		pan.add(bouton10);
+		pan.add(bouton11);
+		pan.add(bouton12);
+		
+		this.setContentPane(pan);
+		this.setVisible(true);
+		
+	}
+	
+	public JButton getbouton1() {
+		return this.bouton1;
+	}
+	
+}
--- YP2/src/Parser.java
+++ YP2/src/Parser.java
@@ -1,7 +1,12 @@
+package src;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.core.internal.utils.FileUtil;
@@ -14,13 +19,32 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import javax.swing.JFrame;
 
-import step2.ASTProcessor;
+//import step2.ASTProcessor;
+
+
+
 
 public class Parser {
 	
-	public static final String projectPath = "/home/dnspc/Desktop/M2/Evo-restru/TP1/Data-Clear/Graal/FichierGraal";
-	public static final String projectSourcePath = projectPath + "/src/";
+	static void gestionGUI() {
+		
+		 Fenetre guiVisitor = new Fenetre();
+		 
+		 
+		 guiVisitor.getbouton1().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				// ICI POUR LANCER UN TRUC VIA LE BOUTON
+				System.out.println("OUI BOUTON");
+			}
+		 });
+	}
+	
+	public static final String projectPath = "C:\\Users\\beaug\\Desktop\\M2\\M2\\Evo-restru\\TP2AST\\HAI913I_Analyse_StatiqueTP\\File_To_Analyse";
+	public static final String projectSourcePath = projectPath;
 	//public static final String projectPath = "/home/hayaat/Desktop/Master/M1/Java/TP4/";
 	//public static final String projectSourcePath = projectPath + "/src/";
 	public static final String jrePath;
@@ -30,6 +54,80 @@ public class Parser {
 	}
 
 	public static void main(String[] args) throws IOException {
+		
+		// MENU
+		int cmd = 100;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		while(cmd != 0) {
+			System.out.println("---- Bienvenue dans notre menu pour obtenir quelques informations sur notre AST ----");
+			System.out.println("Veuillez choisir un numéro de question pour obtenir le résultat");
+			System.out.println("1 - Toutes les questions sur le terminal" );
+			System.out.println("2 - Merci de votre visite, bonne journée !");
+			System.out.println("3 - Nombre de classes de l’application");
+			System.out.println("4 - Nombre de lignes de code de l’application");
+			System.out.println("5 - Nombre total de méthodes de l'application");
+			System.out.println("6 - Nombre total de packages de l'application");
+			System.out.println("7 - Nombre moyen de méthodes par classe");
+			System.out.println("8 - Nombre moyen de lignes de code par méthode");
+			System.out.println("9 - Nombre moyen d'attributs par classe");
+			System.out.println("10 - Les 10% des classes qui possèdent le plus grand nombre de méthodes.");
+			System.out.println("11 - Les 10% des classes qui possèdent le plus grand nombre d'attributs");
+			System.out.println("12 - Les classes qui font partie en même temps des deux catégories précédentes");
+			System.out.println("13 - Les 10% des méthodes qui possèdent le plus grand nombre de lignes de code (par classe)");
+			System.out.println("14 - Le nombre	maximal	de paramètres par rapport à toutes les méthodes	de l’application.");
+			System.out.println("15 - Version graphique");
+			
+			
+			cmd = sc.nextInt();
+			switch(cmd) {
+			
+				case 0 : System.out.println("Merci de votre visite, bonne journée !");
+				break;
+				case 1 : System.out.println("Nombre	de classes de l’application");
+				break;
+				case 3 : System.out.println("Nombre	de lignes de code de l’application");
+				break;
+				case 4 : System.out.println("Nombre total de méthodes de l'application");
+				break;
+				case 5 : System.out.println("Nombre total de packages de l'application");
+				break;
+				case 6 : System.out.println("Nombre moyen de méthodes par classe");
+				break;
+				case 7 : System.out.println("Nombre moyen de lignes de code par méthode");
+				break;
+				case 8 : System.out.println("Nombre moyen d'attributs par classe");
+				break;
+				case 9 : System.out.println("Les 10% des classes qui possèdent le plus grand nombre de méthodes");
+				break;
+				case 10 : System.out.println("Les 10% des classes qui possèdent le plus grand nombre d'attributs");
+				break;
+				case 11 : System.out.println("Les classes qui font partie en même temps des deux catégories précédentes");
+				break;
+				case 12 : System.out.println("Les 10% des méthodes qui possèdent le plus grand nombre de lignes de code (par classe)");
+				break;
+				case 13 : System.out.println("Le nombre	maximal	de paramètres par rapport à toutes les méthodes	de l’application.");
+				break;
+				case 14 : System.out.println("Toutes les réponses précédentes");
+				break;
+				case 15 :
+				System.out.println("La fenêtre va s'ouvrir...");
+				gestionGUI();
+				int i = 1;
+				System.out.println("Appuyez sur une 0 pour revenir au menu");
+				i = sc.nextInt();
+				System.out.println();
+				if (i == 0) {
+				break;
+				}
+			}
+		}
+		
+		
+		// END MENU
+
 		// read java files
 		final File folder = new File(projectSourcePath);
 		ArrayList<File> javaFiles = listJavaFilesForFolder(folder);
@@ -44,6 +142,12 @@ public class Parser {
 			//ASTProcessor processor = new ASTProcessor(parse, "StatVisitor");
 			processor.print();
 		}
+		
+		// TODO : GUI part 
+	
+				 
+				 
+	    // END : GUI part		 
 		
 	}
 
@@ -84,3 +188,5 @@ public class Parser {
 		return (CompilationUnit) parser.createAST(null); // create and parse
 	}
 }
+
+
